1530e1dabc1053a73f1622a5046f7632d7c71149
malak119 <M_malak-2014@hotmail.com>
2021-07-30 01:15:07 +0300

    Creating CSS file for the Attributes which created by using JavaScript and make some changes on index.html file and main.js #5

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,33 +1,33 @@
+const root = document.getElementById("root");
+const logo = document.createElement("img");
+logo.src = "./assets/NHTSA_500.jpg";
+root.appendChild(logo);
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+root.appendChild(container);
 
-let container=document.querySelector(".container")
-
-fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetWMIsForManufacturer/hon?format=json').then(res=>res.json()).then(data=> (
-
-data.Results.forEach(veh => {
-
-
-// console.log(veh.Name)
-// console.log(veh.Country)
-
-let box=document.createElement('rect');
-box.setAttribute("class","box");
-container.appendChild(box);
-
-let boxTitle=document.createElement("div");
-boxTitle.setAttribute("class","boxTitle");
-box.appendChild(boxTitle);
-boxTitle.textContent=veh.Name;
-
-let boxContent=document.createElement("div");
-boxContent.setAttribute("class","boxContent");
-box.appendChild(boxContent);
-boxContent.textContent=`Country: ${veh.Country}`;
-
-
-
-})
-
+fetch(
+  "https://vpic.nhtsa.dot.gov/api/vehicles/GetWMIsForManufacturer/hon?format=json"
 )
+  .then((res) => res.json())
+  .then((data) =>
+    data.Results.forEach((veh) => {
+      // console.log(veh.Name)
+      // console.log(veh.Country)
 
-)
-.catch(err=>console.log(` Error source is ${err}`))
+      let box = document.createElement("rect");
+      box.setAttribute("class", "box");
+      container.appendChild(box);
+
+      let boxTitle = document.createElement("div");
+      boxTitle.setAttribute("class", "boxTitle");
+      box.appendChild(boxTitle);
+      boxTitle.textContent = veh.Name;
+
+      let boxContent = document.createElement("div");
+      boxContent.setAttribute("class", "boxContent");
+      box.appendChild(boxContent);
+      boxContent.textContent = `Country: ${veh.Country}`;
+    })
+  )
+  .catch((err) => console.log(` Error source is ${err}`));
